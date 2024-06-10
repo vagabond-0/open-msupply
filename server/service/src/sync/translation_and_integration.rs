594366e9499d903a33f0a4f9b9bfe7d1b17c42fa
Andrei <andrei.evguenov@gmail.com>
2024-06-10 12:59:43 +1200

    Testing integration

--- server/service/src/sync/translation_and_integration.rs
+++ server/service/src/sync/translation_and_integration.rs
@@ -255,9 +255,13 @@ impl TranslationAndIntegrationResults {
 
 #[cfg(test)]
 mod test {
-    use super::*;
-    use repository::mock::MockDataInserts;
-    use util::{assert_matches, inline_init};
+    use repository::{
+        mock::{MockData, MockDataInserts},
+        test_db, ItemRow, ItemRowRepository, RepositoryError, UnitRow, UnitRowRepository,
+    };
+    use util::{assert_matches, bench_point, bench_results, inline_init, uuid::uuid};
+
+    use crate::sync::{translation_and_integration::integrate, translations::IntegrationOperation};
 
     #[actix_rt::test]
     async fn test_fall_through_inner_transaction() {
@@ -309,5 +313,251 @@ mod test {
             ItemRowRepository::new(&connection).find_active_by_id("item"),
             Ok(None)
         );
+    }
+
+    #[actix_rt::test]
+    async fn test_transaction_speed() {
+        // SQLITE
+
+        // cargo test --package service --lib data/temp.txt -- sync::translation_and_integration::test::test_transaction_speed --exact --nocapture
+        // generate                        PT0S    PT0S
+        // insert                          PT0.277076S     PT0.277076S
+        // done insert                     PT57.376906S    PT57.099830S
+        // insert transaction              PT57.376911S    PT0.000005S
+        // done insert transaction         PT59.649417S    PT2.272506S
+
+        // POSTGRES
+
+        // cargo test --features postgres --package service --lib data/temp.txt -- sync::translation_and_integration::test::test_transaction_speed --exact --nocapture
+        // generate                        PT0S    PT0S
+        // insert                          PT0.281264S     PT0.281264S
+        // done insert                     PT11.619679S    PT11.338415S
+        // insert transaction              PT11.619684S    PT0.000005S
+        // done insert transaction         PT19.922647S    PT8.302963S
+
+        let (_, connection, _, _) = test_db::setup_all(
+            "test_transaction_speed_no_tansaction",
+            MockDataInserts::none(),
+        )
+        .await;
+
+        let (_, connection_transact, _, _) =
+            test_db::setup_all("test_transaction_speed_tansaction", MockDataInserts::none()).await;
+
+        bench_point("generate");
+
+        let records: Vec<UnitRow> = (0..100000)
+            .into_iter()
+            .map(|num| UnitRow {
+                id: uuid(),
+                name: uuid(),
+                description: Some(uuid()),
+                index: num,
+                is_active: true,
+            })
+            .collect();
+
+        bench_point("insert");
+
+        let repo = UnitRowRepository::new(&connection);
+        for record in records.iter() {
+            repo.upsert_one(&record).unwrap();
+        }
+
+        bench_point("done insert");
+
+        bench_point("insert transaction");
+
+        connection_transact
+            .transaction_sync(|con| {
+                let repo = UnitRowRepository::new(&con);
+                for record in records.iter() {
+                    repo.upsert_one(&record).unwrap();
+                }
+
+                Ok(()) as Result<(), ()>
+            })
+            .unwrap();
+
+        bench_point("done insert transaction");
+        bench_results();
+    }
+
+    #[actix_rt::test]
+    async fn test_nested_transaction_and_errors_speed() {
+        // SQLITE
+
+        // cargo test --package service --lib data/temp.txt -- sync::translation_and_integration::test::test_nested_transaction_and_errors_speed --exact --nocapture
+        // generate                        PT0S    PT0S
+        // insert transaction              PT0.094401S     PT0.094401S
+        // done insert transaction         PT4.992747S     PT4.898346S
+        // insert nested transaction       PT4.992756S     PT0.000009S
+        // done insert nested transaction  PT71.597694S    PT66.604938S
+
+        // POSTGRES
+
+        // cargo test --features postgres --package service --lib data/temp.txt -- sync::translation_and_integration::test::test_nested_transaction_and_errors_speed --exact --nocapture
+        // generate                        PT0S    PT0S
+        // insert nested transaction       PT0.129909S     PT0.129909S
+        // done insert nested transaction  PT236.547470S   PT236.417561S
+
+        let unit_row = UnitRow {
+            id: uuid(),
+            ..Default::default()
+        };
+
+        let (_, connection_transact, _, _) = test_db::setup_all_with_data(
+            "test_nested_transaction_and_error_speed_transaction",
+            MockDataInserts::none(),
+            MockData {
+                units: vec![unit_row.clone()],
+                ..Default::default()
+            },
+        )
+        .await;
+
+        let (_, connection_transact_nested, _, _) = test_db::setup_all_with_data(
+            "test_nested_transaction_and_error_speed_nested_transaction",
+            MockDataInserts::none(),
+            MockData {
+                units: vec![unit_row.clone()],
+                ..Default::default()
+            },
+        )
+        .await;
+
+        bench_point("generate");
+
+        let records: Vec<ItemRow> = (0..100000)
+            .into_iter()
+            .map(|num| ItemRow {
+                id: uuid(),
+                // Error in half the records
+                unit_id: if num % 2 == 0 {
+                    Some(unit_row.id.clone())
+                } else {
+                    Some("does not exist".to_string())
+                },
+                ..Default::default()
+            })
+            .collect();
+
+        // Nested transaction will error and exist postgres
+        if cfg!(not(feature = "postgres")) {
+            let mut error_count = 0;
+            bench_point("insert transaction");
+            connection_transact
+                .transaction_sync(|con| {
+                    let repo = ItemRowRepository::new(&con);
+                    for record in records.iter() {
+                        if repo.upsert_one(&record).is_err() {
+                            error_count += 1
+                        }
+                    }
+
+                    Ok(()) as Result<(), ()>
+                })
+                .unwrap();
+
+            assert_eq!(error_count, 50000);
+            bench_point("done insert transaction");
+        }
+
+        // Nested transaction will error
+        if cfg!(feature = "postgres") {
+            let mut error_count = 0;
+            bench_point("insert no transaction");
+            let repo = ItemRowRepository::new(&connection_transact);
+            for record in records.iter() {
+                if repo.upsert_one(&record).is_err() {
+                    error_count += 1
+                }
+            }
+
+            assert_eq!(error_count, 50000);
+            bench_point("done insert no transaction");
+        }
+
+        let mut error_count = 0;
+        bench_point("insert nested transaction");
+        connection_transact_nested
+            .transaction_sync(|con| {
+                for record in records.iter() {
+                    if con
+                        .transaction_sync_etc(
+                            |con| {
+                                let repo = ItemRowRepository::new(&con);
+                                repo.upsert_one(&record)?;
+
+                                Ok(()) as Result<(), RepositoryError>
+                            },
+                            false,
+                        )
+                        .is_err()
+                    {
+                        error_count += 1
+                    }
+                }
+                Ok(()) as Result<(), ()>
+            })
+            .unwrap();
+
+        assert_eq!(error_count, 50000);
+
+        bench_point("done insert nested transaction");
+        bench_results();
+    }
+
+    #[actix_rt::test]
+    async fn test_transaction_fall_through() {
+        // Fall through, in postgres the whole transaction fails, in sqlite just the things that was broken
+        let (_, connection, _, _) =
+            test_db::setup_all("test_transaction_fall_through", MockDataInserts::none()).await;
+
+        let item1 = ItemRow {
+            id: "item1".to_string(),
+            ..Default::default()
+        };
+        let item2 = ItemRow {
+            id: "item2".to_string(),
+            ..Default::default()
+        };
+
+        connection
+            .transaction_sync(|con| {
+                let repo = ItemRowRepository::new(&con);
+
+                repo.upsert_one(&item1).unwrap();
+
+                let error_result = repo.upsert_one(&ItemRow {
+                    id: "error".to_string(),
+                    unit_id: Some("does not exit".to_string()),
+                    ..Default::default()
+                });
+
+                assert!(error_result.is_err());
+
+                let ok_or_error_result = repo.upsert_one(&item2);
+                let aborted = cfg!(feature = "postgres");
+
+                assert_eq!(ok_or_error_result.is_err(), aborted);
+
+                Ok(()) as Result<(), ()>
+            })
+            .unwrap();
+
+        // Check that item1 and item2 exist
+        let repo = ItemRowRepository::new(&connection);
+
+        let item1_from_db = repo.find_one_by_id("item1");
+        let item2_from_db = repo.find_one_by_id("item2");
+
+        if cfg!(feature = "postgres") {
+            assert_eq!(item1_from_db, Ok(None));
+            assert_eq!(item2_from_db, Ok(None));
+        } else {
+            assert_eq!(item1_from_db, Ok(Some(item1)));
+            assert_eq!(item2_from_db, Ok(Some(item2)));
+        }
     }
 }
